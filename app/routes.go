4f3051c34e3f0b1d48e949645e81a294293fbe10
Rifkiyusran <rifki.yusran.tif20@polban.ac.id>
2021-10-15 14:17:19 +0700

    update service

--- app/routes.go
+++
@@ -1,7 +0,0 @@
-package app
-
-import "github.com/Rifkiyusran/B3_Product-Detail-page/app/controllers"
-
-func (server *Server) initializeRoutes() {
-	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
-}
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/Rifkiyusran/B3_Product-Detail-page/app"
+import (
+	"aph-go-service/transport"
+	_ "expvar"
+	"net/http"
+	"os"
+
+	"github.com/go-kit/kit/log"
+)
 
 func main() {
-	app.Run()
+
+	logger := log.NewLogfmtLogger(os.Stdout)
+
+	transport.RegisterHttpsServicesAndStartListener()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	logger.Log("listening-on", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		logger.Log("listen.error", err)
+	}
 }
